8b48fd64101bedb8dd84c1a0c64942c7639ec4d7
Kim Jiwon <kjw100404@gmail.com>
2024-11-27 10:56:05 +0900

    hi

---
+++ link-preview-app/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import LinkPreview from './components/LinkPreview';
+
+const App = () => {
+  const [url, setUrl] = useState('');
+
+  const handleInputChange = (e) => {
+    setUrl(e.target.value);
+  };
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Link Preview</h1>
+      <input
+        type="text"
+        placeholder="Enter a URL"
+        value={url}
+        onChange={handleInputChange}
+        style={{ width: '100%', padding: '10px', marginBottom: '20px' }}
+      />
+      {url && <LinkPreview url={url} />}
+    </div>
+  );
+};
+
+export default App;
